f80e6222ad5c84f97371cbe39e585b82bf05a786
Chamomile Tea <0xE29DA4@gmail.com>
2026-02-06 23:31:04 +0800

    1770391864

---
+++ system/services/dnsmasq.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, lib, ... }: {
+  services.dnsmasq = {
+    enable = true;
+    
+    settings = {
+      interface = "tailscale0";
+      bind-interfaces = true;
+      no-resolv = true;
+      server = [
+        "183.60.83.19"
+        "183.60.82.98"
+        "223.5.5.5"
+      ];
+      cache-size = 1000;
+      domain-needed = true;
+      bogus-priv = true;
+      domain = "vpn.chamomile.icu";
+      local = "/vpn.chamomile.icu/";
+      log-queries = true;
+      log-facility = "/var/log/dnsmasq.log";
+      address = [
+        "/vpn.chamomile.icu/100.111.111.11"
+        "/turing.vpn.chamomile.icu/100.111.111.22"
+        "/ivy.vpn.chamomile.icu/100.111.111.33"
+      ];
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "f /var/log/dnsmasq.log 0644 dnsmasq dnsmasq -"
+  ];
+}
+
