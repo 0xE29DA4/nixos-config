d430cdc06091bf24c17191c4eb027d05b89047ee
Chamomile Tea <me@chamomile.icu>
2026-02-17 18:04:23 +0800

    1771322663

--- system/nix.nix
+++ system/nix.nix
@@ -1,11 +1,13 @@
 {inputs, ...}: {
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nix.settings.substituters = [
+    "https://mirrors.ustc.edu.cn/nix-channels/store"
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://cache.nixos.org/"
     "https://mic92.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "mic92.cachix.org-1:gi8Sa+I96ZB/vjRjEe9nb0mW7qDln9dXBqjoz8K0saQ="
   ];
   nix.settings.auto-optimise-store = true;
