18d68087dea9e55bc7b18f83c749b9af19e70d3e
Chamomile Tea <me@chamomile.icu>
2026-02-25 20:09:30 +0800

    Update

--- users/chamomile/git.nix
+++ users/chamomile/git.nix
@@ -5,7 +5,16 @@
       user.name = "Chamomile Tea";
       user.email = "me@chamomile.icu";
       core.editor = "nvim";
+      core.pager = "delta --paging=always";
       init.defaultBranch = "main";
+      interactive.diffFilter = "delta --color-only";
+      delta.navigate = true;
+      delta.light = false;
+      delta.side-by-side = true;
+      delta.line-numbers = true;
+      delta.features = "decorations";
+      merge.conflictstyle = "zdiff3";
+      alias.dd = "!git difftool -y --extcmd='difft --display side-by-side'";
     };
   };
 }
